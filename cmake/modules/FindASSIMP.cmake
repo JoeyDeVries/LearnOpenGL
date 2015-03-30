344e95db81c80f1bd8a23af8cc819c93dda57b31
zmertens <zmertens@asu.edu>
2015-03-30 15:27:49 -0700

    modified CMake build system

---
+++ cmake/modules/FindASSIMP.cmake
@@ -0,0 +1,30 @@
+# - Try to find Assimp
+# Once done, this will define
+#
+# ASSIMP_FOUND - system has Assimp
+# ASSIMP_INCLUDE_DIR - the Assimp include directories
+# ASSIMP_LIBRARIES - link these to use Assimp
+FIND_PATH( ASSIMP_INCLUDE_DIR assimp/mesh.h
+/usr/include
+/usr/local/include
+/opt/local/include
+)
+FIND_LIBRARY( ASSIMP_LIBRARY assimp
+/usr/lib64
+/usr/lib
+/usr/local/lib
+/opt/local/lib
+)
+IF(ASSIMP_INCLUDE_DIR AND ASSIMP_LIBRARY)
+SET( ASSIMP_FOUND TRUE )
+SET( ASSIMP_LIBRARIES ${ASSIMP_LIBRARY} )
+ENDIF(ASSIMP_INCLUDE_DIR AND ASSIMP_LIBRARY)
+IF(ASSIMP_FOUND)
+IF(NOT ASSIMP_FIND_QUIETLY)
+MESSAGE(STATUS "Found ASSIMP: ${ASSIMP_LIBRARY}")
+ENDIF(NOT ASSIMP_FIND_QUIETLY)
+ELSE(ASSIMP_FOUND)
+IF(ASSIMP_FIND_REQUIRED)
+MESSAGE(FATAL_ERROR "Could not find libASSIMP")
+ENDIF(ASSIMP_FIND_REQUIRED)
+ENDIF(ASSIMP_FOUND)
---
+++ cmake/modules/FindSOIL.cmake
@@ -0,0 +1,33 @@
+# - Locate SOIL library
+# This module defines
+# SOIL_LIBRARY, the name of the library to link against
+# SOIL_FOUND
+# SOIL_INCLUDE_DIR, where to find SOIL.h
+# To Adding search path, set SOIL_ROOT_DIR as follows
+# set(SOIL_ROOT_DIR "path/to/soil")
+# or launch cmake with -DSOIL_ROOT_DIR="/path/to/SOIL_ROOT_DIR".
+#
+# author: Kazunori Kimura
+# email : kazunori.abu@gmail.com
+find_path(SOIL_INCLUDE_DIR_TMP SOIL.h
+HINTS ${SOIL_ROOT_DIR}
+PATH_SUFFIXES include/SOIL SOIL include
+)
+if(${SOIL_INCLUDE_DIR_TMP} STREQUAL "SOIL_INCLUDE_DIR_TMP-NOTFOUND")
+set(SOIL_INCLUDE_DIR ${SOIL_INCLUDE_DIR_TMP})
+else()
+string(REGEX REPLACE "(.*)/SOIL" "\\1" SOIL_INCLUDE_DIR ${SOIL_INCLUDE_DIR_TMP})
+endif()
+message("-- SOIL include dirctory " ${SOIL_INCLUDE_DIR})
+find_library(SOIL_LIBRARY
+NAMES SOIL
+HINTS ${SOIL_ROOT_DIR}
+PATH_SUFFIXES lib
+)
+message("-- SOIL Library " ${SOIL_LIBRARY})
+unset(INCLUDE_SEARCH_PATH)
+unset(LIB_SEARCH_PATH)
+unset(SOIL_INCLUDE_DIR_TMP)
+include(FindPackageHandleStandardArgs)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(SOIL
+REQUIRED_VARS SOIL_LIBRARY SOIL_INCLUDE_DIR)
