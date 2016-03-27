d7ed65ff5daaa77d75b03eddcf014ed72dddc6d9
J. de Vries <joey.d.vries@gmail.com>
2016-03-28 01:07:11 +0200

    Shadow mapping view frustum clean-up (more intuitive LookAt Up vector).

--- src/5.advanced_lighting/3.1.shadow_mapping/shadow_mapping.cpp
+++ src/5.advanced_lighting/3.1.shadow_mapping/shadow_mapping.cpp
@@ -164,7 +164,7 @@ int main()
         GLfloat near_plane = 1.0f, far_plane = 7.5f;
         lightProjection = glm::ortho(-10.0f, 10.0f, -10.0f, 10.0f, near_plane, far_plane);
         //lightProjection = glm::perspective(45.0f, (GLfloat)SHADOW_WIDTH / (GLfloat)SHADOW_HEIGHT, near_plane, far_plane); // Note that if you use a perspective projection matrix you'll have to change the light position as the current light position isn't enough to reflect the whole scene.
-        lightView = glm::lookAt(lightPos, glm::vec3(0.0f), glm::vec3(1.0));
+        lightView = glm::lookAt(lightPos, glm::vec3(0.0f), glm::vec3(0.0, 1.0, 0.0));
         lightSpaceMatrix = lightProjection * lightView;
         // - now render scene from light's point of view
         simpleDepthShader.Use();
