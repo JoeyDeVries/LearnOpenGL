d78752800f202c657c8892c59dacf8641c7fc7a9
Joey de Vries <joey.d.vries@gmail.com>
2017-05-28 08:56:30 +0200

    Shader tab fix.

--- includes/learnopengl/shader_m.h
+++ includes/learnopengl/shader_m.h
@@ -141,28 +141,28 @@ private:
     // utility function for checking shader compilation/linking errors.
     // ------------------------------------------------------------------------
     void checkCompileErrors(GLuint shader, std::string type)
-	{
-		GLint success;
-		GLchar infoLog[1024];
-		if(type != "PROGRAM")
-		{
-			glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
-			if(!success)
-			{
-				glGetShaderInfoLog(shader, 1024, NULL, infoLog);
+    {
+        GLint success;
+        GLchar infoLog[1024];
+        if (type != "PROGRAM")
+        {
+            glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+            if (!success)
+            {
+                glGetShaderInfoLog(shader, 1024, NULL, infoLog);
                 std::cout << "ERROR::SHADER_COMPILATION_ERROR of type: " << type << "\n" << infoLog << "\n -- --------------------------------------------------- -- " << std::endl;
-			}
-		}
-		else
-		{
-			glGetProgramiv(shader, GL_LINK_STATUS, &success);
-			if(!success)
-			{
-				glGetProgramInfoLog(shader, 1024, NULL, infoLog);
+            }
+        }
+        else
+        {
+            glGetProgramiv(shader, GL_LINK_STATUS, &success);
+            if (!success)
+            {
+                glGetProgramInfoLog(shader, 1024, NULL, infoLog);
                 std::cout << "ERROR::PROGRAM_LINKING_ERROR of type: " << type << "\n" << infoLog << "\n -- --------------------------------------------------- -- " << std::endl;
-			}
-		}
-	}
+            }
+        }
+    }
 };
 
 #endif
