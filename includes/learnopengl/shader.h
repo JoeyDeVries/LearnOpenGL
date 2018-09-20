ce0c6dab4ec01b6c18db1c7421aee8ac6cec96ea
linesight <ted.lin0000@gmail.com>
2018-09-20 01:23:38 -0700

    fix unreferenced local variable warning

--- includes/learnopengl/shader.h
+++ includes/learnopengl/shader.h
@@ -61,8 +61,6 @@ public:
         const char * fShaderCode = fragmentCode.c_str();
         // 2. compile shaders
         unsigned int vertex, fragment;
-        int success;
-        char infoLog[512];
         // vertex shader
         vertex = glCreateShader(GL_VERTEX_SHADER);
         glShaderSource(vertex, 1, &vShaderCode, NULL);
