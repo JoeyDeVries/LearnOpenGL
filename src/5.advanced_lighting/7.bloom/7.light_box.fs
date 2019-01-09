2300a20eb01236109f15fb7c26a3b77665209159
Joey de Vries <joey.d.vries@gmail.com>
2019-01-09 13:03:22 +0100

    For Bloom tutorial, make sure second MRT target's BrightColor is assigned a default value (appears to break on some drivers/cards).

--- src/5.advanced_lighting/7.bloom/7.light_box.fs
+++ src/5.advanced_lighting/7.bloom/7.light_box.fs
@@ -16,4 +16,6 @@ void main()
     float brightness = dot(FragColor.rgb, vec3(0.2126, 0.7152, 0.0722));
     if(brightness > 1.0)
         BrightColor = vec4(FragColor.rgb, 1.0);
+	else
+		BrightColor = vec4(0.0, 0.0, 0.0, 1.0);
 }
