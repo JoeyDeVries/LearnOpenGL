4b890eca6b44c68d5d729806f9cc665b3ac2be17
Joey de Vries <joey.d.vries@gmail.com>
2020-04-25 16:57:19 +0200

    Update Breakout code for revised Particles chapter.

--- src/7.in_practice/3.2d_game/0.full_source/particle_generator.h
+++ src/7.in_practice/3.2d_game/0.full_source/particle_generator.h
@@ -22,7 +22,7 @@
 struct Particle {
     glm::vec2 Position, Velocity;
     glm::vec4 Color;
-    float Life;
+    float     Life;
 
     Particle() : Position(0.0f), Velocity(0.0f), Color(1.0f), Life(0.0f) { }
 };
