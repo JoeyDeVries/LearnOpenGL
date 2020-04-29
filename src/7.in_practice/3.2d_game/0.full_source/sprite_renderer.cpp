f9ae66b16a150d4ccb745c3bfa86d14d948c1b69
Joey de Vries <joey.d.vries@gmail.com>
2020-04-29 17:40:12 +0200

    Breakout spriterenderer: User-code expects rotation in degrees, make sure to convert to radians during transform generation.

--- src/7.in_practice/3.2d_game/0.full_source/sprite_renderer.cpp
+++ src/7.in_practice/3.2d_game/0.full_source/sprite_renderer.cpp
@@ -28,7 +28,7 @@ void SpriteRenderer::DrawSprite(Texture2D &texture, glm::vec2 position, glm::vec
     model = glm::translate(model, glm::vec3(position, 0.0f));  // first translate (transformations are: scale happens first, then rotation, and then final translation happens; reversed order)
 
     model = glm::translate(model, glm::vec3(0.5f * size.x, 0.5f * size.y, 0.0f)); // move origin of rotation to center of quad
-    model = glm::rotate(model, rotate, glm::vec3(0.0f, 0.0f, 1.0f)); // then rotate
+    model = glm::rotate(model, glm::radians(rotate), glm::vec3(0.0f, 0.0f, 1.0f)); // then rotate
     model = glm::translate(model, glm::vec3(-0.5f * size.x, -0.5f * size.y, 0.0f)); // move origin back
 
     model = glm::scale(model, glm::vec3(size, 1.0f)); // last scale
