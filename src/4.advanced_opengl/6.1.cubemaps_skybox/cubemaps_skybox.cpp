31c2cf3a9af7831250cec84d9122ddc46201c4d7
SnowyLake <snowylake.dev@outlook.com>
2021-11-09 17:26:20 +0800

    bugfix: In cubemaps_skybox.cpp, the second argument to glBufferDelete() should be &SkyboxVBO instead of &SkyboxVAO

--- src/4.advanced_opengl/6.1.cubemaps_skybox/cubemaps_skybox.cpp
+++ src/4.advanced_opengl/6.1.cubemaps_skybox/cubemaps_skybox.cpp
@@ -275,7 +275,7 @@ int main()
     glDeleteVertexArrays(1, &cubeVAO);
     glDeleteVertexArrays(1, &skyboxVAO);
     glDeleteBuffers(1, &cubeVBO);
-    glDeleteBuffers(1, &skyboxVAO);
+    glDeleteBuffers(1, &skyboxVBO);
 
     glfwTerminate();
     return 0;
