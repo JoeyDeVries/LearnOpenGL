76b3e8535216c8429b2153c0be819fea853d425a
N. Pattakos <pattakosn@fastmail.com>
2022-01-07 23:09:38 +0100

    fix unused exception parameter: print it in exception message

--- includes/learnopengl/shader_m.h
+++ includes/learnopengl/shader_m.h
@@ -43,7 +43,7 @@ public:
         }
         catch (std::ifstream::failure& e)
         {
-            std::cout << "ERROR::SHADER::FILE_NOT_SUCCESFULLY_READ" << std::endl;
+            std::cout << "ERROR::SHADER::FILE_NOT_SUCCESFULLY_READ: " << e.what() << std::endl;
         }
         const char* vShaderCode = vertexCode.c_str();
         const char * fShaderCode = fragmentCode.c_str();
