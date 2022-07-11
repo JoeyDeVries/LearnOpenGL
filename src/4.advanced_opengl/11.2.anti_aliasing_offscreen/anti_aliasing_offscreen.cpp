6159792dec67ff0ba70f7fd2eafd88b683730e64
Joey de Vries <joey.d.vries@gmail.com>
2022-07-11 16:51:04 +0200

    Fix correct shader activation on AA chapter.

--- src/4.advanced_opengl/11.2.anti_aliasing_offscreen/anti_aliasing_offscreen.cpp
+++ src/4.advanced_opengl/11.2.anti_aliasing_offscreen/anti_aliasing_offscreen.cpp
@@ -200,7 +200,7 @@ int main()
 
     // shader configuration
     // --------------------
-    shader.use();
+    screenShader.use();
     screenShader.setInt("screenTexture", 0);
 
     // render loop
