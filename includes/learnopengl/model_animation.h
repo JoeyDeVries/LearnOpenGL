53538069bc0f56677a26d2e4c2490ab166db8316
J.G.Adams <82924084+JG-Adams@users.noreply.github.com>
2022-08-02 17:24:21 -0400

    Reinsert #include <stb_image.h>
    The problem might just have been mine! :)

--- includes/learnopengl/model_animation.h
+++ includes/learnopengl/model_animation.h
@@ -5,6 +5,9 @@
 
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
+
+#include <stb_image.h>
+
 #include <assimp/Importer.hpp>
 #include <assimp/scene.h>
 #include <assimp/postprocess.h>
