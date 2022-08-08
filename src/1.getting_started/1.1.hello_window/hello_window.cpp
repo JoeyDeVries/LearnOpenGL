dedafdc5048738c5826f5c5928db1ecb538476cd
dreamblue98 <117737222@qq.com>
2022-08-08 21:40:13 +0800

    Self-Opengl-Test

--- src/1.getting_started/1.1.hello_window/hello_window.cpp
+++ src/1.getting_started/1.1.hello_window/hello_window.cpp
@@ -14,7 +14,7 @@ int main()
 {
     // glfw: initialize and configure
     // ------------------------------
-    //first   example
+    //Self-Opengl-test
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
