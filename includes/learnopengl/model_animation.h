e26db64ce5fc070ff6f694fa3c316eca3bc27845
J.G.Adams <82924084+JG-Adams@users.noreply.github.com>
2022-08-18 01:57:22 -0400

    Didn't mean to change this. So I revert it back

--- includes/learnopengl/model_animation.h
+++ includes/learnopengl/model_animation.h
@@ -85,10 +85,7 @@ private:
     {
         // read file via ASSIMP
         Assimp::Importer importer;
-        const aiScene* scene = importer.ReadFile(path, aiProcess_Triangulate
-                                                 //| aiProcess_GenSmoothNormals
-                                                 | aiProcess_FlipUVs | aiProcess_CalcTangentSpace
-                                                 );
+        const aiScene* scene = importer.ReadFile(path, aiProcess_Triangulate | aiProcess_GenSmoothNormals | aiProcess_CalcTangentSpace);
         // check for errors
         if(!scene || scene->mFlags & AI_SCENE_FLAGS_INCOMPLETE || !scene->mRootNode) // if is Not Zero
         {
