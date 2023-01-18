b58c94480e446767447586732948bf2342a98d9b
Josh Soref <2119212+jsoref@users.noreply.github.com>
2023-01-18 08:35:00 -0500

    spelling: rotation
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- includes/learnopengl/entity.h
+++ includes/learnopengl/entity.h
@@ -28,10 +28,10 @@ protected:
 		const glm::mat4 transformZ = glm::rotate(glm::mat4(1.0f), glm::radians(m_eulerRot.z), glm::vec3(0.0f, 0.0f, 1.0f));
 
 		// Y * X * Z
-		const glm::mat4 roationMatrix = transformY * transformX * transformZ;
+		const glm::mat4 rotationMatrix = transformY * transformX * transformZ;
 
 		// translation * rotation * scale (also know as TRS matrix)
-		return glm::translate(glm::mat4(1.0f), m_pos) * roationMatrix * glm::scale(glm::mat4(1.0f), m_scale);
+		return glm::translate(glm::mat4(1.0f), m_pos) * rotationMatrix * glm::scale(glm::mat4(1.0f), m_scale);
 	}
 public:
 
