1acd9c17bbc361b49b90efa30502174c9c977c72
Josh Soref <2119212+jsoref@users.noreply.github.com>
2023-01-17 23:27:51 -0500

    spelling: into
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- src/8.guest/2021/3.tessellation/terrain_cpu_src/8.3.cpuheight.fs
+++ src/8.guest/2021/3.tessellation/terrain_cpu_src/8.3.cpuheight.fs
@@ -6,6 +6,6 @@ in float Height;
 
 void main()
 {
-    float h = (Height + 16)/32.0f;	// shift and scale the height in to a grayscale value
+    float h = (Height + 16)/32.0f;	// shift and scale the height into a grayscale value
     FragColor = vec4(h, h, h, 1.0);
 }
