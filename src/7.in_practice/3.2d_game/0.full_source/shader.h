216723a8b039cbfa0ffeaa990256ff206e59633f
Josh Soref <2119212+jsoref@users.noreply.github.com>
2023-01-18 08:34:55 -0500

    spelling: purpose
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- src/7.in_practice/3.2d_game/0.full_source/shader.h
+++ src/7.in_practice/3.2d_game/0.full_source/shader.h
@@ -16,7 +16,7 @@
 #include <glm/gtc/type_ptr.hpp>
 
 
-// General purpsoe shader object. Compiles from file, generates
+// General purpose shader object. Compiles from file, generates
 // compile/link-time error messages and hosts several utility 
 // functions for easy management.
 class Shader
