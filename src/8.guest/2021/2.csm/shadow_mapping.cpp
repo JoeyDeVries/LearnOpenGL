c4d704f14c658e27b5628fb7459ef6847eaf1a53
David Siegel <agnat@me.com>
2023-05-16 22:34:44 +0200

    Touch-up: Actually request v4.1

--- src/8.guest/2021/2.csm/shadow_mapping.cpp
+++ src/8.guest/2021/2.csm/shadow_mapping.cpp
@@ -72,8 +72,8 @@ int main()
     // glfw: initialize and configure
     // ------------------------------
     glfwInit();
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
 #ifdef __APPLE__
