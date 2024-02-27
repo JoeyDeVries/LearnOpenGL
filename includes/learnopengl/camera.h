5390505e6f48f24d7a339ac3ae374a654f3da0cd
elmarsan <mselias97@gmail.com>
2024-02-27 22:21:56 +0100

    remove vector include

--- includes/learnopengl/camera.h
+++ includes/learnopengl/camera.h
@@ -5,8 +5,6 @@
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 
-#include <vector>
-
 // Defines several possible options for camera movement. Used as abstraction to stay away from window-system specific input methods
 enum Camera_Movement {
     FORWARD,
